5be18a1c29275d9500e0d0660bd2c6ee62e47223
tonycq <152169867+tonycq@users.noreply.github.com>
2024-02-10 14:49:58 +0000

    proyecto terminado dom

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".com", ".org", ".cl"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let r = 0; r < adj.length; r++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let j = 0; j < dom.length; j++) {
+          console.log(pronoun[i] + adj[r] + noun[n] + dom[j]);
+        }
+      }
+    }
+  }
 };
